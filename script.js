f67c0f6ff676a92d5915ecf6a151d8b3ea9a2c48
yogeshghorela-iitd <yogeshghorela1707@gmail.com>
2025-09-05 09:16:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Theme Toggle
+const toggleBtn = document.getElementById('theme-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  toggleBtn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+});
